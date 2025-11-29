9c4a46bde12dfc16d777b159b1c07e8a03e1de96
HyeJin003 <rkddkwnl003@gmail.com>
2025-11-30 01:33:06 +0900

    Feat: todoList 검색 및 리스트 렌더링하기 기능 추가

--- react/todoList/src/assets/components/Header.jsx
+++ react/todoList/src/assets/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
   return (
     <div className="Header">
       <h3>오늘은😘 </h3>
-      <h2>{new Date().toDateString()}</h2>
+      <h2>{new Date().toLocaleDateString()}</h2>
     </div>
   );
 };
