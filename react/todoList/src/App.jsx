afb097ce769dd878cad8e253e991ba0e345bebaf
HyeJin003 <rkddkwnl003@gmail.com>
2025-11-29 21:08:17 +0900

    feat: 기능 구현 준비하기

--- react/todoList/src/App.jsx
+++ react/todoList/src/App.jsx
@@ -1,8 +1,33 @@
 import "./App.css";
-import Header from "./assets/components/Header";
-import List from "./assets/components/List";
-import Editor from "./assets/components/Editor";
+import Header from "./assets/components/Header.jsx";
+import List from "./assets/components/List.jsx";
+import Editor from "./assets/components/Editor.jsx";
+import { useState } from "react";
 function App() {
+  const mockData = [
+    {
+      id: 0,
+      Done: false,
+      content: "Ract 공부하기",
+      // date: new Date(mockData[0].date).toLocaleDateString(),
+      date: new Date().getTime(),
+    },
+    {
+      id: 1,
+      Done: false,
+      content: "빨래하기",
+      // date: new Date(mockData[0].date).toLocaleDateString(),
+      date: new Date().getTime(),
+    },
+    {
+      id: 2,
+      Done: false,
+      content: "노래하기",
+      // date: new Date(mockData[0].date).toLocaleDateString(),
+      date: new Date().getTime(),
+    },
+  ];
+  const [todos, setTodos] = useState(mockData);
   return (
     <div className="App">
       <Header></Header>
